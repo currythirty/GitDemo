15c234a0320b622fb0c28cb39a715d4d7186ecd6
curry <1036313890@qq.com>
2021-02-23 01:35:51 +0800

    第一次提交代码！

---
+++ src/main/java/com/itheima/pojo/Student.java
@@ -0,0 +1,12 @@
+package com.itheima.pojo;
+
+public class Student {
+    private String name;
+    private String age;
+
+    public Student(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
